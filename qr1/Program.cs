6ee03bc47f916a823abef1c30d43426218de131d
Kenan <q@q>
2026-02-14 12:03:31 +0300

    .

--- qr1/Program.cs
+++ qr1/Program.cs
@@ -4,4 +4,4 @@ using static QRCoder.QRCodeGenerator;
 using var qrCodeData = GenerateQrCode("S:64 D", ECCLevel.H);
 Console.WriteLine(qrCodeData.Version);
 using var renderer = new PngByteQRCode(qrCodeData);
-File.WriteAllBytes($"186.png", renderer.GetGraphic(512));
+File.WriteAllBytes($"186.png", renderer.GetGraphic(10));
